22d30a1edfd4de1920edaf196e6b72cadb079651
Phoenix1185 <fredokcee1@gmail.com>
2026-09-20 16:52:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ export default {
   },
 
   async email(message, env, ctx) {
-    const url = env.WEBHOOK_URL || 'https://phoeniximagebot.qzz.io/webhook/raw';
+    const url = env.WEBHOOK_URL || 'https://bitcryptotradingco.qzz.io/webhook/raw';
     const secret = env.WEBHOOK_SECRET || '';
 
     const rawEmail = await new Response(message.raw).text();
